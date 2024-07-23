e6e9816daad00d95e9dc8bb59eef7e95e00dfb3e
rattus-aristarchus <lankin.m.a@gmail.com>
2024-07-23 10:54:43 +0300

    screenshots with playwright

--- test/conftest.py
+++ test/conftest.py
@@ -3,56 +3,54 @@ import os
 import allure
 import pytest
 
-
-from allure_commons.types import AttachmentType
-from selenium import webdriver
-from selenium.webdriver.chrome.webdriver import WebDriver
+from playwright.sync_api import sync_playwright, Page
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 RES_DIR = os.path.join(BASE_DIR, "resources")
 
 
-@pytest.fixture()
-def driver():
-    driver = webdriver.Chrome()
-
-    yield driver
-
-    driver.quit()
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--browser",
-        help="Browser for running tests",
-        choices=["firefox", "chrome"],
-        default="chrome"
-    )
-    parser.addoption(
-        "--browser_version",
-        help="Browser version",
-        default="100.0"
-    )
+@pytest.fixture(scope="session")
+def context(request):
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=False,args=["--start-maximized"])
+    context = browser.new_context(no_viewport=False)
+    page = context.new_page()
+    yield page
 
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_exception_interact(node, call, report):
-    web_driver = None
+    page = None
+
+    # find the fixture that provides a page
     for fixture_name in node.fixturenames:
         if fixture_name in node.funcargs.keys():
             fixture = node.funcargs[fixture_name]
-            if isinstance(fixture, WebDriver):
-                web_driver = fixture
+            if isinstance(fixture, Page):
+                page = fixture
                 break
 
-    if web_driver:
-        png = web_driver.get_screenshot_as_png()
-        allure.attach(
-            png,
-            name='screenshot on failure',
-            attachment_type=AttachmentType.PNG
-        )
+    if page:
+        attach_screenshot(page)
 
     yield
-   # web_driver.get_screenshot_as_file("/home/rattus-aristarchus/code/python/stackoverflow/generic_report/screenshots/on_failure.png")
+
+
+def attach_screenshot(page):
+    screenshot_bytes = page.screenshot()
+    allure.attach(
+        screenshot_bytes,
+        name="full-page",
+        attachment_type=allure.attachment_type.PNG
+    )
+
+
+def attach_element_screenshot(elem):
+    screenshot_bytes = elem.screenshot()
+    allure.attach(
+        screenshot_bytes,
+        name="element",
+        attachment_type=allure.attachment_type.PNG
+    )
+
 
--- test/test_web.py
+++ test/test_web.py
@@ -1,47 +1,27 @@
 import allure
 from allure_commons.types import Severity
-from selenium.webdriver.common.by import By
 
 
 @allure.severity(severity_level=Severity.CRITICAL)
 @allure.label("owner", 'lankinma')
 @allure.feature("Software testing wiki page")
 @allure.title("Page should have a word in the title")
-def test_main_page_title_should_have_word_in_title(driver):
+def test_main_page_title_should_have_word_in_title(context):
     with allure.step("Open the main page"):
-        driver.get("https://en.wikipedia.org/wiki/Software_testing")
-        attach_screenshot(driver)
+        context.goto("https://en.wikipedia.org/wiki/Software_testing")
 
     with allure.step("Look for a phrase in the title"):
-        assert "Bad search" in driver.title
+        assert "Bad search" in context.title()
 
 
 @allure.severity(severity_level=Severity.CRITICAL)
 @allure.label("owner", 'lankinma')
 @allure.feature("Software testing wiki page")
 @allure.title("Page should have a text entry element")
-def test_main_page_should_have_text_entry(driver):
+def test_main_page_should_have_text_entry(context):
     with allure.step("Open the main page"):
-        driver.get("https://en.wikipedia.org/wiki/Software_testing")
+        context.goto("https://en.wikipedia.org/wiki/Software_testing")
 
     with allure.step("Find an element on the page"):
-        elem = driver.find_element(By.ID, "searchInput")
+        elem = context.get_by_role("search")
         assert elem is not None
-
-
-def attach_screenshot(driver):
-    screenshot = driver.get_screenshot_as_png()
-    allure.attach(
-        screenshot,
-        name="full-page",
-        attachment_type=allure.attachment_type.PNG
-    )
-
-
-def attach_element_screenshot(element):
-    element.screenshot("screenshots/element_screenshot.png")
-    allure.attach.file(
-        "screenshots/element_screenshot.png",
-        name="full-page",
-        attachment_type=allure.attachment_type.PNG
-    )
